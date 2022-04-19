2ebf34f6ec6c0f397031946a34803c5a34012c00
Sebastian Gracia <sebasgraciavalderrama@users.noreply.github.com>
2022-04-19 15:17:48 -0500

    guess my number project completed

--- Dom&Events/GuessMyNumber/script.js
+++ Dom&Events/GuessMyNumber/script.js
@@ -1,46 +1,76 @@
 'use-strict';
-const secretNumber = Math.trunc((Math.random() * 20)) + 1;
+const displayMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+}
+
+const displayNumber =  function (number) {
+    document.querySelector('.number').textContent = String(number);
+}
+
+const displayScore =  function (score) {
+    document.querySelector('.score').textContent = String(score);
+}
+
+const displayHighScore =  function (highScore) {
+    document.querySelector('.highscore').textContent = String(highScore);
+}
+
+const changeStyles = function (option){
+    if (option) { // Won the game
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+    } else { // Play again
+        document.querySelector('body').style.backgroundColor = '#222';
+        document.querySelector('.number').style.width = '15rem';
+    }
+}
+
+const displayGuess = function (guess) {
+    document.querySelector('.guess').value = guess;
+}
+
+const secretNumberFunc = function () {
+    return Math.trunc((Math.random() * 20)) + 1;
+}
+
+let secretNumber = secretNumberFunc();
 let score = 20;
 let highScore = 0;
-
 console.log(secretNumber);
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     if (!guess) {
-        document.querySelector('.message').textContent = 'No number!';
+        displayMessage('No number!');
     } else if (secretNumber === guess){
-        document.querySelector('.message').textContent = 'Correct number!';
-        score++;
-        document.querySelector('.score').textContent = String(score);
-        highScore++;
-        document.querySelector('.highscore').textContent = String(highScore);
-    } else if (guess > secretNumber){
-        if (score > 1) {
-            document.querySelector('.message').textContent = 'Too high!';
-            score--;
-            document.querySelector('.score').textContent = String(score);
-        } else {
-            document.querySelector('.message').textContent = 'You lost the game!';
-            document.querySelector('.score').textContent = String(0);
-        }
-    } else if (guess < secretNumber){
-        if (score > 1) {
-            document.querySelector('.message').textContent = 'Too low!';
-            score--;
-            document.querySelector('.score').textContent = String(score);
-        } else {
-            document.querySelector('.message').textContent = 'You lost the game!';
-            document.querySelector('.score').textContent = String(0);
+        changeStyles(true);
+        displayMessage('Correct number!');
+        displayNumber(secretNumber);
+        displayScore(score);
+        if (score > highScore) {
+            highScore = score;
+            displayHighScore(highScore);
         }
     } else if (guess !== secretNumber){
         if (score > 1) {
-            document.querySelector('.message').textContent = 'Try again!';
+            displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!')
             score--;
-            document.querySelector('.score').textContent = String(score);
+            displayScore(score);
         } else {
-            document.querySelector('.message').textContent = 'You lost the game!';
-            document.querySelector('.score').textContent = String(0);
+            displayMessage('You lost the game!');
+            displayScore(0);
         }
     }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20
+    displayScore(score);
+    secretNumber = secretNumberFunc();
+    console.log(secretNumber);
+    displayMessage('Start guessing...');
+    displayScore(0);
+    changeStyles(false);
+    displayNumber('?');
+    displayGuess('');
 });
