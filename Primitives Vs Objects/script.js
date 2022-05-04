ad07d27abfc58360ac10c1be5b2001fa76d898a3
Sebastian Gracia <sebastiangracia123@gmail.com>
2022-05-04 14:38:50 -0500

    final theory session

--- Primitives Vs Objects/script.js
+++ Primitives Vs Objects/script.js
@@ -1,16 +1,39 @@
 'use strict'
+// Primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
 
-let age = 30;
-let oldAge = age;
-age = 31;
+console.log(lastName, oldLastName); // Davis Williams
 
-const me = {
-    name: 'Sebas',
-    age: 30,
+// Reference types
+const jessica = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 28,
 };
 
-const friend = me;
-friend.age = 27;
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage:', jessica);
+console.log('After marriage:', marriedJessica);
 
-console.log('Friend:', friend);
-console.log('Me:', me);
+//marriedJessica = {}; this does not work since the variable has been declared as CONST, let solves the issue
+
+// Copying objects
+const jessica2 = {
+    firstName: 'Jessica Copy',
+    lastName: 'Williams',
+    age: 28,
+    family: ['Alice', 'Bob']
+};
+
+const jessicaCopy = Object.assign({}, jessica2); // Shallow copy and not a deep clone (everything)
+jessicaCopy.lastName = 'Davis';
+console.log('Before marriage:', jessica2);
+console.log('After marriage:', jessicaCopy);
+
+jessicaCopy.family.push('Mary New');
+jessicaCopy.family.push('John New');
+console.log('Before marriage:', jessica2);
+console.log('After marriage:', jessicaCopy);
