801eb3dd84eed9a5f74fe435563bb6423968b636
Sebastian Gracia <sebastiangracia123@gmail.com>
2022-05-11 10:16:05 -0500

    Coding challenge #1

---
+++ DataStructures, Modern Operators and Strings/Coding Challenges/#1/script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski'
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze'
+        ]
+    ],
+    score: '4.0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
+};
+
+const players = [...game.players[0], ...game.players[1]];
+console.log(players.length);
+
+const [gk, ...fieldPlayers] = [game.players[0][0], ...game.players];
+console.log(gk);
+console.log(fieldPlayers.length);
+console.log(fieldPlayers);
