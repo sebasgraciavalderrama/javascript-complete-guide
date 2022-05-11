32c50f4935ce49091ca1b269d99a7ca2129e31a3
Sebastian Gracia <sebasgraciavalderrama@users.noreply.github.com>
2022-05-11 11:20:46 -0500

    Coding challenge #1 solved and for-of loops

--- DataStructures, Modern Operators and Strings/Coding Challenges/#1/script.js
+++ DataStructures, Modern Operators and Strings/Coding Challenges/#1/script.js
@@ -41,10 +41,36 @@ const game = {
     },
 };
 
-const players = [...game.players[0], ...game.players[1]];
-console.log(players.length);
+//1.
+const [players1, players2] = game.players;
+console.log(players1, players2);
 
-const [gk, ...fieldPlayers] = [game.players[0][0], ...game.players];
+//2. Remaining - REST
+const [gk, fieldPlayers] = players1;
 console.log(gk);
-console.log(fieldPlayers.length);
 console.log(fieldPlayers);
+
+//3.
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+//4.
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
+console.log(players1Final);
+
+//5.
+const {odds: {team1, x: draw, team2}} = game;
+console.log(team1, team2, draw);
+
+//6.
+const printGoals = function (...players) {
+    console.log(players);
+    console.log(`${players.length} goals were scored`);
+}
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals('Davies', 'Muller');
+printGoals(...game.scored);
+
+//7.
+team1 < team2 && console.log('Team 1 is more likely to win!');
+team1 > team2 && console.log('Team 2 is more likely to win!');
---
+++ DataStructures, Modern Operators and Strings/For-of Loop/script.js
@@ -0,0 +1,57 @@
+'use strict';
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0, // Open 24 hours
+            close: 24,
+        },
+    },
+
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+    orderDelivery: function ({starterIndex = 1, mainIndex= 0, time ='14:50' , address}) {
+        console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered at ${time} to ${address} `);
+    },
+    orderPasta: function (ing1, ing2, ing3) {
+        console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+    },
+    orderPizza: function (mainIngredient, ...otherIngredients) {
+        console.log(`Here is your delicious pizza with ${mainIngredient}`);
+        if (otherIngredients.length > 0) {
+            console.log(`Additional ingredients: ${otherIngredients.length}`);
+            console.log(...otherIngredients);
+        }
+    }
+};
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+console.log('Printing menu....');
+for (const item of menu) {
+    console.log(item);
+}
+console.log('Printing menu For-of loop....');
+for (const item of menu.entries()) {
+    console.log(`${item[0] + 1}: ${item[1]}`);
+}
+/*
+for (const [i, el] of menu.entries()) {
+    console.log(`${i + 1}: ${el}`);
+}*/
+console.log([...menu.entries()]);
+
