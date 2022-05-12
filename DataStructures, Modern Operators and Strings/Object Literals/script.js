1384ed71982c3af89da80f7c5e554822c415725b
Sebastian Gracia <sebastiangracia123@gmail.com>
2022-05-12 11:57:31 -0500

    object literals and optional chaining

---
+++ DataStructures, Modern Operators and Strings/Object Literals/script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const weekDays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+    [weekDays[3]]: {
+        open: 12,
+        close: 22,
+    },
+    [weekDays[4]]: {
+        open: 11,
+        close: 23,
+    },
+    [`day-${2+4}`]: {
+        open: 0, // Open 24 hours
+        close: 24,
+    },
+};
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    // ES6 enhanced object literals
+    openingHours,
+
+    order(starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+    orderDelivery({starterIndex = 1, mainIndex= 0, time ='14:50' , address}) {
+        console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered at ${time} to ${address} `);
+    },
+    orderPasta(ing1, ing2, ing3) {
+        console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+    },
+    orderPizza(mainIngredient, ...otherIngredients) {
+        console.log(`Here is your delicious pizza with ${mainIngredient}`);
+        if (otherIngredients.length > 0) {
+            console.log(`Additional ingredients: ${otherIngredients.length}`);
+            console.log(...otherIngredients);
+        }
+    }
+};
+
+console.log(openingHours);
