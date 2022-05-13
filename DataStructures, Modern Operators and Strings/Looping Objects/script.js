8a53d733c7117327797d87c3816656004aa6afa7
Sebastian Gracia <sebastiangracia123@gmail.com>
2022-05-13 12:21:28 -0500

    Looping objects, option chaining

--- DataStructures, Modern Operators and Strings/Looping Objects/script.js
+++ DataStructures, Modern Operators and Strings/Looping Objects/script.js
@@ -45,31 +45,27 @@ const restaurant = {
     }
 };
 
-// With optional chaining
-console.log(restaurant.openingHours.mon?.open);
-console.log(restaurant.openingHours?.mon?.open);
+const properties = Object.keys(openingHours);
 
-// Example
-const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+console.log(properties);
+let openStr = (`We are open on ${properties.length} days: `);
 
-for (const day of days) {
-    const open = restaurant.openingHours[day]?.open ?? 'closed';
-    console.log(`On ${day}, we open at ${open}`);
+for (const day of properties) {
+    openStr += `${day}, `;
+}
+console.log(openStr);
+
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire object
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+// [key, value] -> Destructuring
+for (const [key, {open, close}] of entries) {
+    console.log(`On ${key} we open at ${open} and close at ${close}`);
 }
 
-// Methods
-console.log(restaurant.order?.(0,1) ?? 'Method does not exit!');
-console.log(restaurant.orderRisotto?.(0.1) ?? 'Method does not exit!');
 
-// Arrays
-const users = [
-    {name: 'Sebas', email: 'email@email.com'},
-    {name: 'Mona', email: 'email@email.com'},
-];
-
-console.log(users[0]?.name ?? 'User array empty!');
-if (users.length > 0) {
-    console.log(users[0]?.name);
-} else {
-    console.log('User array empty!');
-}
