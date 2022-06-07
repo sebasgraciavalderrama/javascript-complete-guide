3185b00c64aee6b2a5d7b5032a43878a2a800f1f
Sebastian Gracia <sebastiangracia123@gmail.com>
2022-06-07 14:27:05 -0500

    code challenge solution

--- Functions/Coding challenge/script.js
+++ Functions/Coding challenge/script.js
@@ -4,27 +4,33 @@ const poll = {
     question: 'Waht is your favorite programming language?',
     options: ['0: Javascript', '1: Python', '2: Rust', '3: C++'],
     answers: new Array(4).fill(0),
-    registerNewAnswer () {
+    consoleLoggedQuestion () {
         console.log('What is your favourite programming language?');
         for (let answer of this.options){
             console.log(answer);
         }
         console.log('(Write option number)')
     },
-    increasePollAnswer (answer = 2) {
-        if (answer) {
-
-            console.log(this.answers[answer]);
-            this.answers[answer] = 1;
-            console.log(this.answers);
+    answerPollQuestion () {
+        const choice = Number(prompt('What is your favourite programming language?'));
+        if (choice in [0, 1, 2, 3]) {
+            console.log(this.answers[choice]);
+            this.answers[choice] += 1;
         } else {
             console.log('Please input a valid option (0,1,2,3)');
         }
+
+        this.displayResults();
+        this.displayResults('string');
     },
+    displayResults(type = 'array') {
+        if (type === 'array') {
+            console.log(this.answers);
+        } else if (type === 'string') {
+            console.log(`Poll results are ${this.answers.join(', ')}`)
+        }
+    }
 };
-poll.registerNewAnswer();
 
-const choice = Number(prompt('What is your favourite programming language?'));
-document.querySelector('.poll').addEventListener('click', poll.increasePollAnswer(choice).bind(poll)); //TODO
-
-
+document.querySelector('.poll').addEventListener('click', poll.answerPollQuestion.bind(poll));
+poll.consoleLoggedQuestion();
